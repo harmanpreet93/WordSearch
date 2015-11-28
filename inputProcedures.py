fe82d976913da89a3afbb0b29dd33f5405352fdb
harmanpreet93 <cs12b1017@iith.ac.in>
2015-11-28 16:04:23 +0530

    binarization step implemeneted

---
+++ inputProcedures.py
@@ -0,0 +1,100 @@
+from myImports import *
+
+def getSearchWord():
+	searchWord = raw_input("Enter the word to search from image: ")
+	return searchWord
+
+def readInputImage():
+	inputImg = raw_input("Please provide the input image to search: ")
+	# print "Image path you entered is: ", inputImg
+
+	# Check if valid image path and valid image extension
+	if not validateImage(inputImg):
+		exit(0)
+
+	# If valid image path and valid image extension, then go ahead
+	# Read input image and word to search here
+	colorFlag = True
+	return cv2.imread(inputImg,colorFlag)
+
+	# Plot image
+	# plotImage(inputImg)
+
+def validateImage(inputImg):
+	validImageExts = ['.bmp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.jpeg', '.jpg', 
+	    				'.jpe', '.jp2', '.tiff', '.tif', '.png']
+	
+	# Check if input image has valid path
+	if not os.path.exists(inputImg):
+		print "*****IMAGE NOT FOUND*****"
+		return False
+		
+	else:
+		# Check if input image has valid extension
+		if not os.path.splitext(inputImg)[1] in validImageExts:
+			print "*****NOT A VALID IMAGE EXTENSION*****"
+			return False
+		return True
+
+def convertToGrayScale(image):
+	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def applyFilter(image,filter):
+	ksize = (3,3)
+	if filter == 'gaussianBlur':
+		return applyGaussianFilter(image,ksize)
+	
+	elif filter == 'medianBlur':
+		return applyMedianFilter(image,ksize[0])
+	
+	return applyMedianFilter(image,ksize[0])
+
+def applyGaussianFilter(image,ksize):
+	sigmaX = 0
+	return cv2.GaussianBlur(image,ksize,sigmaX)
+
+def applyMedianFilter(image,ksize):
+	return cv2.medianBlur(image, ksize)
+
+def applyBinarization(image,thresholdType):
+	if thresholdType == 'simple':
+		return simpleThresholding(image)
+
+	elif thresholdType == 'adaptive':
+		return adaptiveThresholding(image)
+
+	elif thresholdType == 'otsu':
+		return otsuThresholding(image)
+
+	return adaptiveThresholding(image)
+
+def simpleThresholding(image):
+	threshold = 127
+	maxValue = 255
+	thresholdType = cv2.THRESH_BINARY_INV
+	ret,outputImg = cv2.threshold(image,threshold,maxValue,thresholdType)
+	return outputImg 
+
+def adaptiveThresholding(image):
+	maxValue = 255
+	adaptiveMethod = cv2.ADAPTIVE_THRESH_MEAN_C
+	thresholdType = cv2.THRESH_BINARY_INV
+	# block size can vary
+	blockSize = 51
+	subtractionConst = 10
+	return cv2.adaptiveThreshold(image, maxValue, adaptiveMethod, thresholdType, blockSize,subtractionConst) 
+
+def otsuThresholding(image):
+	threshold = 0
+	maxValue = 255
+	thresholdType = cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU
+	ret,outputImg = cv2.threshold(image,threshold,maxValue,thresholdType)
+	return outputImg 
+
+def plotImage(inputImg):
+	cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+	cv2.imshow('Image',inputImg)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+
---
+++ mainFile.py
@@ -0,0 +1,208 @@
+from inputProcedures import *
+
+def drawBoxes(img,boundRect):
+    color = (0,0,255)
+    for i in range(0,len(boundRect)):
+        pt1 = (boundRect[i][0],boundRect[i][1])
+        pt2 = (boundRect[i][0]+boundRect[i][2],boundRect[i][1]+boundRect[i][3])
+        cv2.rectangle(img,pt1,pt2,color,1,8,0)
+
+    plotImage(img)
+
+def labelLettersWithBox(letterWithBox,contours):
+    numContours = len(contours)
+    boundRect = []
+
+    color = (0,0,255)
+
+    for i in range(0,numContours):
+        ans = cv2.boundingRect(contours[i])
+        boundRect.append(ans)
+        # cv2.rectangle(letterWithBox,boundRect[i],color,1,8,0)
+        pt1 = (ans[0],ans[1])
+        pt2 = (ans[0]+ans[2],ans[1]+ans[3])
+        cv2.rectangle(letterWithBox,pt1,pt2,color,1,8,0)
+    # plotImage(letterWithBox)
+    return boundRect
+
+def findCharSize(boundRect,numLetters):
+    areaMap = {}
+    numRects = len(boundRect)
+    for i in range(0,numRects):
+        area = boundRect[i][2]*boundRect[i][3]
+        if areaMap.has_key(area):
+            areaMap[area]+=1
+        else:
+            areaMap[area] = 1
+
+    sorted_area = sorted(areaMap.items(), key=lambda x:x[1],reverse=True)
+    # sorted_area = sorted(areaMap,key=areaMap.get)
+    # print sorted_area[0:20]
+    areaAvg = 0.0
+    for key in sorted_area[0:10]:
+        areaAvg += key[0]
+    areaAvg = areaAvg / 10.0;
+    cHeight = (areaAvg) **0.5 * 1.1;
+    cWidth = (areaAvg) **0.5 * 0.75;
+    cArea = areaAvg;
+    widthLimit = numLetters * cWidth;
+
+    return areaAvg,cHeight,cWidth,widthLimit
+
+def mergeBox(boundRect,cHeight,cWidth):
+    emptyRect = [0,0,0,0]
+    for i in range(0,len(boundRect)):
+        # check for boundRect if already set to nullRect
+        for j in range(i+1,len(boundRect)):
+            if (boundRect[i] != emptyRect and boundRect[j] != emptyRect) and isNeighbour(boundRect[i],boundRect[j],cHeight,cWidth):
+                mergeBoundRect(boundRect,i,j)
+                break
+
+def mergeBoundRect(boundRect,i,j):
+    mergedRect = (boundRect[i][0], boundRect[i][1], boundRect[j][0]+boundRect[j][2]-boundRect[i][0], max(boundRect[i][3],boundRect[j][3]) )
+    #store x,y,w,h for an empty rectangle, i.e., a point
+    emptyRect = [0,0,0,0]
+    boundRect[i] = emptyRect
+    boundRect[j] = emptyRect
+    boundRect.append(mergedRect)
+
+
+def clearNullRect(boundRect,cArea):
+    boundRect = [x for x in boundRect if (x[2]*x[3] != 0 and x[2]*x[3] >= 0.35*cArea and x[2]*x[3] <= 22*cArea)]
+
+def isNeighbour(rect1,rect2,cHeight,cWidth):
+
+    if rect1[2]*rect2[3] == 0 or rect2[2]*rect2[3] == 0:
+        return False
+
+    y1 = rect1[1]+(rect1[1]+rect1[3])
+    y2 = rect2[1]+(rect2[1]+rect2[3])
+
+    dy = abs(y1 - y2)/2
+
+    dy1 = abs(rect1[1] - rect2[1]);
+    dy2 = abs(rect1[1]+rect1[3] - (rect2[1]+rect2[3]))
+
+    dx1 = abs(rect1[0] - (rect2[0]) + rect2[2])
+    dx2 = abs(rect1[0] + rect1[2] - rect2[0])
+
+    # if ((rect1 & rect2).area() != 0 ):
+    #   return true
+    
+    l1 = [rect1[0],rect1[1]]
+    r1 = [l1[0]+rect1[2],l1[1]+rect1[3]]
+    l2 = [rect2[0],rect2[1]]
+    r2 = [l2[0]+rect2[2],l2[1]+rect2[3]]
+    
+    # two dx is because when the bounding box becomes a rectangule, the original dx will not work anymore
+    # two rectangles intersect
+    if checkOverlap(l1,r1,l2,r2):
+        return True
+
+    if ((dy < 0.65 * cHeight or dy1 < 0.28 * cHeight or dy2 < 0.32 * cHeight)  and (dx1 < 0.31 * cWidth or dx2 < 0.31 *cWidth)):
+        return True
+    else:
+        return False
+
+# Returns true if two rectangles (l1, r1) and (l2, r2) overlap
+def checkOverlap(l1,r1,l2,r2):
+
+     # If one rectangle is on left side of other
+    if (l1[0] > r2[0] or l2[0] > r1[0]):
+        return False
+
+    # If one rectangle is above other
+    if (l1[1] < r2[1] or l2[1] < r1[1]):
+        return False
+
+    return True
+
+if __name__ == "__main__":
+    # Read input user image from getInputImage
+    inputImg = readInputImage()
+
+    # Ask user for the word to search
+    searchWord = getSearchWord()
+    print "Search-Word:",searchWord
+
+    # Copy of image
+    copyImage = np.copy(inputImg)
+    numLetters = len(searchWord)
+
+    # convert image to grayScale and filter image
+    grayImage = convertToGrayScale(inputImg)
+    
+    # apply filter
+        # - medianBlur or gaussianBlur
+        # - no argument applies medianBlur
+    filteredImage = applyFilter(grayImage,'gaussianBlur')
+
+    # perform binarization
+        # - simple, adaptive or otsu
+        # - no argument applies adaptive
+    binarizedImg = applyBinarization(filteredImage,'otsu')
+
+    # plotImage(binarizedImg)
+
+    # skipped deskew wala part for now
+
+    # in else of extract_text.cpp
+    # print binarizedImg.shape
+    size = binarizedImg.shape[0],binarizedImg.shape[1],3
+    blankImage = np.zeros(size, dtype=np.uint8)
+
+    bwImageForTess = np.copy(binarizedImg)
+
+    # bwimage is binarizedImg
+    wordWithBox = cv2.cvtColor(binarizedImg,cv2.COLOR_GRAY2RGB)
+
+    ret,thresh = cv2.threshold(filteredImage,127,255,0)
+    outputImg,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    outputImg = cv2.drawContours(filteredImage,contours,-1,(0,255,0),3)
+
+    # plotImage(outputImg)
+
+    letterWithBox = np.copy(wordWithBox);
+
+    # searchAndLabelWord(resultImage, bb_mask, wordWithBox, bwImageForTess, boundRect, wordToSearch, widthLimit);
+    # // output result
+    # //imwrite("result_image.jpg", resultImage);
+    # imwrite("./output/bb_mask.jpg", bb_mask);
+    # imwrite("./output/result_image.jpg", resultImage);
+
+    boundRect = labelLettersWithBox(letterWithBox, contours)
+
+    cHeight,cWidth,cArea,widthLimit = findCharSize(boundRect,numLetters)
+
+    # print cHeight
+
+    mergeBox(boundRect,cHeight,cWidth)
+    clearNullRect(boundRect,cArea)
+
+    drawBoxes(inputImg,boundRect)
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
